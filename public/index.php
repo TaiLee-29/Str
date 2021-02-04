62bbe8e415e734397276250de87e739e43c0044e
TaiLee-29 <dreih796@gmail.com>
2021-02-04 16:46:32 +0200

    Normul)

--- public/index.php
+++ public/index.php
@@ -1,2 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
+
+var_dump(Hillel\str::after('My name is Ibragim', 'name'));
+    ?>
--- src/str.php
+++ src/str.php
@@ -6,5 +6,22 @@ namespace Hillel;
 
 class str
 {
+    public static function after($subject, $after){
+        $subjectPos = strpos($subject,$after);
+        if( $subjectPos == false){
+          return $subject;
+       } elseif ($subjectPos){
+            return substr($subject,$subjectPos+strlen($after));
+        }
+
+    }
+
+
+
+
+
+
+
+
 
 }
